02c37e4197d175b63626326a06e6d42d426c1159
plshah24678 <78969723+plshah24678@users.noreply.github.com>
2021-06-14 08:10:44 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+var feed,lastFed,fedTime;
+
+//create feed and lastFed variable here
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  feed=createButton("Feed The Dog");
+  feed.position(900,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  if(lastFed>=12){
+    //show time in PM format when Last Fed is greater than 12
+  }else if(lastFed==0){
+    text("Last Feed : 12AM",350,30);
+  }else{
+   // show time in AM format when Last Fed is less than 12
+  }
+ 
+  //write code to display text lastFed time here
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  var food_stock_val=foodObj.getFoodStock();
+  if(food_stock_val<=0){
+    foodObj.updateFoodStock(food_stock_val*0);
+  }else{
+    foodObj.updateFoodStock(food_stock_val-1);
+  }
+  //write code here to update food stock and last fed time
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
